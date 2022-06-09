6274bd4b8143ad4d6e339d7072b5b120a20b758e
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2022-06-09 00:27:33 -0400

    create home, login and register routes to render relevant ejs

--- app.js
+++ app.js
@@ -8,7 +8,18 @@ const app = express();
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function(req, res) {
+  res.render("home");
+});
+
+app.get("/login", function(req, res) {
+  res.render("login");
+});
  
+app.get("/register", function(req, res) {
+  res.render("register");
+});
  
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
