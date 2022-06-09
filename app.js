f99d0d86816d31266ce9a1d101e51aae2a081515
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2022-06-09 12:39:48 -0400

    Installed mongoose-encryption plugin and required the encrypt module in app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+var encrypt = require('mongoose-encryption');
 
 const PORT = 3000;
  
