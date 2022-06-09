6cfe86c81b92e24125d86887277bd7efbde8b104
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2022-06-09 11:59:13 -0400

    Fixed the bug allowing to see 'secrets' page after registering

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+// MODULES:
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 
-const port = 3000;
+const PORT = 3000;
  
 const app = express();
  
@@ -11,19 +12,23 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+/////////////////////////////CONNECTION TO MONGODB///////////////////////
 // connect to MongoDB
 main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect("mongodb://localhost:27017/userDB");
 }
 
+// a Schema for userDB
 const userSchema = {
   email: String,
   password: String
 };
 
+// a Mongoose Model
 const User = new mongoose.model("User", userSchema);
 
+/////////////////////////////GET REQUESTS/////////////////////////////
 app.get("/", function(req, res) {
   res.render("home");
 });
@@ -36,20 +41,22 @@ app.get("/register", function(req, res) {
   res.render("register");
 });
 
+///////////////////////////////POST REQUESTS////////////////////////////
 app.post("/register", function(req, res) {
   // reading from inputs:
   const username = req.body.username;
   const password = req.body.password;
-  // creating a new data entry to our db:
+  // creating a new data entry to our db when a user registering:
   const newUser = new User({
     email: username,
     password: password
   });
   // saving a new data entry to our db and dealing with err, if any:
   newUser.save(function(err) {
-    if (!err) {
-      console.log("Successfully registered");
+    if (err) {
+      console.log(err);
     } else {
+      // we are rendering the 'secrets' page unless the user is registered or logged in
       res.render("secrets");
     }
   });
@@ -58,6 +65,7 @@ app.post("/register", function(req, res) {
 app.post("/login", function(req, res) {
   const username = req.body.username;
   const password = req.body.password;
+  // we are filtering through our db searching for the user using a username and a password
   User.findOne({email: username}, function(err, foundUser) {
     if(err) {
       console.log(err);
@@ -71,6 +79,7 @@ app.post("/login", function(req, res) {
   })
 })
  
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+// PORT is listening
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
