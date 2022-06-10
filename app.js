ae4e6740e438db9fb02279d5c2ea2a99fe64563f
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2022-06-10 15:22:09 -0400

    Required the package

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-find-or-create')
 
 const PORT = 3000;
  
